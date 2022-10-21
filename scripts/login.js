a8899f956a2ccee5135a3e77654b0648b93a0cee
Adrielfgs <adrielgalvao.fernandes@gmail.com>
2022-10-20 23:50:07 -0300

    Check13

--- scripts/login.js
+++ scripts/login.js
@@ -1,6 +1,6 @@
 function user() {
     document.querySelector(".box3").style.opacity = '100';
-    document.querySelector(".box3").style.height = '100px' ;
+    document.querySelector(".box3").style.height = '100px';
     document.querySelector(".login").style.height = '80px';
     document.querySelector(".base").style.height = '0px';
     document.querySelector(".base").style.opacity = '0';
@@ -20,10 +20,21 @@ function adm() {
     document.querySelector(".box3").style.height = '0px';
     document.querySelector(".box3").style.opacity = '0';
     document.querySelector(".login").style.opacity = '100';
-    var input = document.querySelector("#email");
+    var input = document.querySelector("#id");
     input.disabled = false;
     var input2 = document.querySelector("#password");
     input2.disabled = false;
     var button = document.querySelector("#log");
     button.disabled = false;
+}
+
+function login() {
+    var id = document.getElementById('id').value
+    if (id == 223344) {
+        console.log(id)
+        window.location.href = "/content.html";
+    }
+    else {
+        console.log("Senha Errada!")
+    }
 }
